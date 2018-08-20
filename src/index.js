c83ee97c2f35a2278742629513ca92b597b788d3
kai021998 <kai.han@balliol.ox.ac.uk>
2018-08-20 11:09:14 -0400

    finished

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // your code here!
+  const commentsForm = document.getElementById("comment-form")
+  commentsForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const commentsInput = document.getElementById("add-comments-input")
+    const addComment = comment => {
+      const ul = document.getElementById("comments-list")
+      ul.innerHTML += `
+        <li>
+        ${comment.value}
+        <button class="delete-button" type="submit" name="action"> X </button>
+        </li>
+      `
+    }
+    addComment(commentsInput)
+    commentsInput.value = ""
+  })
+
+  const filterInput = document.getElementById('filter-comments-input')
+  // the filter functionality is case sensitive, which was a purposeful decision //
+  filterInput.addEventListener("keyup", event => {
+    const filterValue = event.target.value
+    const comments = document.getElementById("comments-list")
+    const commentsList = comments.getElementsByTagName("li")
+    for (let item of commentsList) {
+      if (item.textContent.includes(filterValue)) {
+        item.style.display = ""
+      } else {
+        item.style.display = "none"
+      }
+    }
+  })
+
+  const deleteButtons = document.getElementsByClassName("delete-button")
+  // started trying to build out delete button functionality, but saw I couldn't add an event listener to an html collection //
+
+  // deleteButtons.addEventListener("click", event => {
+  //   event.preventDefault()
+  //   console.log(event)
+  // })
 })
--- src/vanillaJS.js
+++ src/vanillaJS.js
@@ -1,3 +1,32 @@
 document.addEventListener('DOMContentLoaded', function() {
-  // your code here!
+  const commentsForm = document.getElementById("comment-form")
+  commentsForm.addEventListener("submit", event => {
+    event.preventDefault();
+    const commentsInput = document.getElementById("add-comments-input")
+    const addComment = comment => {
+      const ul = document.getElementById("comments-list")
+      ul.innerHTML += `
+        <li>
+        ${comment.value}
+        </li>
+      `
+    }
+    addComment(commentsInput)
+    commentsInput.value = ""
+  })
+
+  const filterInput = document.getElementById('filter-comments-input')
+  // the filter functionality is case sensitive, which was a purposeful decision //
+  filterInput.addEventListener("keyup", event => {
+    const filterValue = event.target.value
+    const comments = document.getElementById("comments-list")
+    const commentsList = comments.getElementsByTagName("li")
+    for (let item of commentsList) {
+      if (item.textContent.includes(filterValue)) {
+        item.style.display = ""
+      } else {
+        item.style.display = "none"
+      }
+    }
+  })
 })
